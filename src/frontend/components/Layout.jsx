644027f5816c2be596362da711f0aab3fcb333a4
amoslover <amoslover@163.com>
2021-04-17 13:17:27 +0800

    fix getAmountOut

--- src/frontend/components/Layout.jsx
+++ src/frontend/components/Layout.jsx
@@ -88,8 +88,8 @@ class Layout extends Component{
         <Route path="/newtoken" exact render={() => (
           <ProtectedRouteWrap component={<TokenIssueForm />} access={this.state.hasUser} redirectPath="/connectwallet" />
         )} />
-        <Route path="/test" exact render={() => <Test />} />
-        {/* <Route path="/connectwallet" exact render={() => <KeyPair changeUser={val => this.setState({ hasUser: val })} />} /> */}
+        {/* <Route path="/test" exact render={() => <Test />} /> */}
+        <Route path="/connectwallet" exact render={() => <KeyPair changeUser={val => this.setState({ hasUser: val })} />} />
       </div>
     )
   }
