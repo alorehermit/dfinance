5f1ab42bce4b4dd7aeed805895b000034d0baf64
amoslover <amoslover@163.com>
2021-04-19 18:58:02 +0800

    update token-select

---
+++ src/frontend/components/SwapRelated/AddLiquidity.jsx
@@ -0,0 +1,376 @@
+import BigNumber from "bignumber.js";
+import React, { Component } from "react";
+import { addLiquidity, approveToken, getDTokenBalance, getPair, getTokenAllowance } from "../../APIs/token.js";
+import canister_ids from "../../utils/canister_ids.json";
+import InputGroup from "../Inputs/InputGroup.jsx";
+import TokenSelect from "../Inputs/TokenSelect.jsx";
+
+const DSWAP_CANISTER_ID = canister_ids.dswap.local;
+
+class AddLiquidity extends Component {
+  constructor() {
+    super();
+    this.state = {
+      approved: false,
+      token0Amount: "",
+      token0Bal: "",
+      token0: null,
+      token0Error: false,
+      token1Amount: "",
+      token1Bal: "",
+      token1: null,
+      token1Error: false,
+      showTokenList: false,
+      bigger: false,
+      loading: "",
+      pairInfo: null
+    };
+  }
+
+  _isMounted = false;
+  componentDidMount() {
+    this._isMounted = true;
+  }
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.token0 && this.state.token0.canisterId !== (prevState.token0 ? prevState.token0.canisterId : "")) {
+      this.getTokenBalance(this.state.token0.canisterId, this.state.token0.decimals)
+        .then(res => {
+          if (this._isMounted) this.setState({ token0Bal: res });
+        });
+    }
+    if (this.state.token1 && this.state.token1.canisterId !== (prevState.token1 ? prevState.token1.canisterId : "")) {
+      this.getTokenBalance(this.state.token1.canisterId, this.state.token1.decimals)
+        .then(res => {
+          if (this._isMounted) this.setState({ token1Bal: res });
+        });
+    }
+    if (this.props.tokens && this.props.tokens.length > 0 && !this.state.token0) {
+      this.setState({ token0: this.props.tokens[0] });
+    }
+    if (
+      this.state.token0 && 
+      this.state.token1 && 
+      (
+        (JSON.stringify(this.state.token0) !== JSON.stringify(prevState.token0)) ||
+        (JSON.stringify(this.state.token1) !== JSON.stringify(prevState.token1))
+      )
+    ) {
+      this.updatePairInfo();
+    }
+    if (
+      this.state.token0 && 
+      this.state.token1 && 
+      (
+        (JSON.stringify(this.state.token0) !== JSON.stringify(prevState.token0)) ||
+        (JSON.stringify(this.state.token1) !== JSON.stringify(prevState.token1))
+      ) &&
+      !this.state.approved
+    ) {
+      Promise.all([
+        getTokenAllowance(this.state.token0.canisterId, DSWAP_CANISTER_ID, this.state.token0.decimals),
+        getTokenAllowance(this.state.token1.canisterId, DSWAP_CANISTER_ID, this.state.token1.decimals)
+      ])
+        .then(([res1, res2]) => {
+          console.log("allowance: ", res1, res2);
+          if (parseFloat(res1) > 0 && parseFloat(res2) > 0 && this._isMounted) {
+            this.setState({ approved: true });
+          }
+        })
+        .catch(err => {
+          console.log("get token0 or token1 allowance failed");
+          console.log(err);
+        });
+    }
+  }
+
+  updatePairInfo = () => {
+    if (this.state.token0 && this.state.token1) {
+      getPair(this.state.token0.canisterId, this.state.token1.canisterId)
+        .then(res => {
+          if (this._isMounted) this.setState({ pairInfo: res[0] });
+        })
+        .catch(err => {
+          console.log("get pair failed");
+          console.log(err);
+        });
+    }
+  }
+  updateBals = () => {
+    if (this.state.token0 && this.state.token0.canisterId) {
+      this.getTokenBalance(this.state.token0.canisterId, this.state.token0.decimals)
+        .then(res => {
+          if (this._isMounted) this.setState({ token0Bal: res });
+        });
+    }
+    if (this.state.token1 && this.state.token1.canisterId) {
+      this.getTokenBalance(this.state.token1.canisterId, this.state.token1.decimals)
+        .then(res => {
+          if (this._isMounted) this.setState({ token1Bal: res });
+        });
+    }
+  }
+  getTokenOptions = (token, tokens, pairs) => {
+    if (!token) return tokens;
+    let res = [];
+    for (let i = 0; i < pairs.length; i++) {
+      if (pairs[i].indexOf(token.canisterId) === 0) {
+        res = res.concat(tokens.filter(el => el.canisterId === pairs[i][1]));
+      } else if (pairs[i].indexOf(token.canisterId) === 1) {
+        res = res.concat(tokens.filter(el => el.canisterId === pairs[i][0]));
+      }
+    }
+    return res;
+  };
+  token0OnSelect = token0 => {
+    this.setState({ token0 });
+    if (!this.state.token1) return;
+    if (token0.canisterId === this.state.token1.canisterId) {
+      return this.setState({ token1: null, token1Bal: "", token1Amount: "", bigger: false });
+    }
+    const num = this.props.pairs.filter(i => 
+      i.indexOf(token0.canisterId) > -1 && i.indexOf(this.state.token1.canisterId) > -1
+    ).length;
+    if (num === 0) {
+      this.setState({ token1: null, token1Bal: "", token1Amount: "", bigger: false });
+    }
+  };
+  getTokenBalance = async (canisterId, decimals) => {
+    try {
+      const bal = await getDTokenBalance(canisterId, decimals);
+      console.log("12: ", bal)
+      return bal;
+    } catch(err) {
+      console.log(err);
+      return "0";
+    }
+  };
+  quote = (amount, r0, r1) => {
+    if (
+      amount > new BigNumber("0"),
+      r0 > new BigNumber("0"),
+      r1 > new BigNumber("0")
+    ) {
+      return amount
+        .multipliedBy(r1)
+        .dividedBy(r0);
+    } else {
+      return new BigNumber("0");
+    }
+  };
+  calculateAmount1 = () => {
+    if (!this.state.pairInfo || !this.state.token0 || !this.state.token1) return;
+    const { reserve0, reserve1 } = this.state.pairInfo;
+    console.log(reserve0, reserve1);
+    if(reserve0 === "0" && reserve1 === "0") {
+      return;
+    } else {
+      let res;
+      const amount1Optimal = this.quote(
+        new BigNumber(this.state.amount0)
+          .multipliedBy(new BigNumber("10")
+          .pow(parseInt(this.state.token0.decimals))),
+        new BigNumber(reserve0), 
+        new BigNumber(reserve1)
+      );
+      if(amount1Optimal <= new BigNumber(this.state.amount1)) {
+        res = amount1Optimal
+          .dividedBy(new BigNumber("10")
+          .pow(parseInt(this.state.token1.decimals)))
+          .toString();
+      } else {
+        res = this.state.amount1;
+      };
+      this.setState({ amount1: res });
+    };
+  };
+  calculateAmount0 = () => {
+    if (!this.state.pairInfo || !this.state.token0 || !this.state.token1) return;
+    const { reserve0, reserve1 } = this.state.pairInfo;
+    if(reserve0 === "0" && reserve1 === "0") {
+      return;
+    } else {
+      let res;
+      const amount0Optimal = this.quote(
+        new BigNumber(this.state.amount1)
+          .multipliedBy(new BigNumber("10")
+          .pow(parseInt(this.state.token1.decimals))),
+        new BigNumber(reserve0), 
+        new BigNumber(reserve1)
+      );
+      if(amount0Optimal <= new BigNumber(this.state.amount0)) {
+        res = amount0Optimal
+          .dividedBy(new BigNumber("10")
+          .pow(parseInt(this.state.token0.decimals)))
+          .toString();
+      } else {
+        res = this.state.amount0;
+      };
+      this.setState({ amount0: res });
+    };
+  };
+  token0AmountOnChange = e => {
+    const val = e.target.value;
+    const reg = new RegExp(/^[0-9\.]*$/);
+    if (val && !reg.test(val)) return;
+    this.setState({ token0Amount: val }, () => {
+      // this.calculateAmount1();
+    });
+    if (!val || !parseFloat(val)) return this.setState({ token0Error: true });
+    if (this.state.token0Bal && parseFloat(this.state.token0Bal) < parseFloat(val)) return this.setState({ token0Error: true });
+    this.setState({ token0Error: false });
+  };
+  token1AmountOnChange = e => {
+    const val = e.target.value;
+    const reg = new RegExp(/^[0-9\.]*$/);
+    if (val && !reg.test(val)) return;
+    this.setState({ token1Amount: val }, () => {
+      // this.calculateAmount0();
+    });
+    if (!val || !parseFloat(val)) return this.setState({ token1Error: true });
+    if (this.state.token1Bal && parseFloat(this.state.token1Bal) < parseFloat(val)) return this.setState({ token1Error: true });
+    this.setState({ token1Error: false });
+  };
+  approve = async () => {
+    this.setState({ loading: "Approving..." });
+    const MAX_AMOUNT = Number.MAX_SAFE_INTEGER;
+    try {
+      await approveToken(
+        this.state.token0.canisterId, 
+        DSWAP_CANISTER_ID, 
+        MAX_AMOUNT, 
+        this.state.token0.decimals
+      );
+      await approveToken(
+        this.state.token1.canisterId, 
+        DSWAP_CANISTER_ID, 
+        MAX_AMOUNT, 
+        this.state.token1.decimals
+      );
+    } catch (err) {
+      console.log("approve token0 or token1 failed");
+      console.log(err);
+    }
+    if (this._isMounted) this.setState({ loading: "Done" }, () => {
+      setTimeout(() => {
+        if (this._isMounted) this.setState({ loading: "", approved: true });
+      }, 1500);
+    });
+  };
+  add = () => {
+    this.setState({ loading: "Adding..." });
+    addLiquidity(
+      this.state.token0.canisterId, 
+      this.state.token1.canisterId, 
+      this.state.token0Amount, 
+      this.state.token1Amount,
+      this.state.token0.decimals, 
+      this.state.token1.decimals
+    )
+      .then(() => {
+        if (this._isMounted) {
+          this.updateBals();
+          this.updatePairInfo();
+        }
+      })
+      .catch(err => {
+        console.log("add liquidity failed");
+        console.log(err);
+      }) 
+      .finally(() => {
+        if (this._isMounted) this.setState({ loading: "Done" }, () => {
+          setTimeout(() => {
+            if (this._isMounted) this.setState({ loading: "" });
+          }, 1500);
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div className="SwapLiquidityPage SwapLiquidityPage2">
+        <button className="back" onClick={() => this.props.goPage(0)}>{"< Back"}</button>
+        <label className="label">Add Liquidity</label>
+        <InputGroup
+          label="Input"
+          placeholder="0.00"
+          value={this.state.token0Amount}
+          onChange={this.token0AmountOnChange}
+          token={this.state.token0}
+          balance={this.state.token0Bal}
+          onSelect={token0 => this.token0OnSelect(token0)}
+          options={this.props.tokens}
+          err={this.state.token0Error}
+          class="token0"
+        />
+        <div className="plus-svg">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22">
+            <path id="路径_78" data-name="路径 78" d="M-1482.83,785.25h-8.284v-8.284h-5.431v8.284h-8.284v5.431h8.284v8.284h5.431v-8.284h8.284Z" transform="translate(1504.83 -776.966)" fill="#b8b8b8"/>
+          </svg>
+        </div>
+        {this.state.token1 ?
+          <InputGroup
+            label="Input"
+            placeholder="0.00"
+            value={this.state.token1Amount}
+            onChange={this.token1AmountOnChange}
+            token={this.state.token1}
+            balance={this.state.token1Bal}
+            onSelect={token1 => this.setState({ token1 })}
+            options={this.getTokenOptions(this.state.token0, this.props.tokens, this.props.pairs)}
+            err={this.state.token1Error}
+            class="token1"
+          /> :
+          <TokenSelect
+            label="Input"
+            options={this.getTokenOptions(this.state.token0, this.props.tokens, this.props.pairs)}
+            onSelect={token1 => this.setState({ token1 })}
+            setBigger={bigger => this.setState({ bigger })}
+          />
+        }
+        {!this.state.bigger || this.state.token1 ? 
+          <div className="swap-info">
+            <span> </span>
+          </div>
+        : null}
+        {this.state.approved ? 
+          this.state.loading ?
+            <button className="submit" disabled>{this.state.loading}</button> :
+            <button className="submit" onClick={this.add} disabled={
+              !this.state.token1 || 
+              !this.state.token0Amount || 
+              !this.state.token1Amount || 
+              this.state.token0Error || 
+              this.state.token1Error ||
+              parseFloat(this.state.token0Amount || "0") * parseFloat(this.state.token1Amount || "0") <= 1000 * 1000
+            }>
+              Add Liquidity
+            </button>
+        :
+          this.state.loading ? 
+            <button className="submit" disabled>{this.state.loading}</button> :
+            <button className="submit" onClick={this.approve} disabled={
+              !this.state.token1 || 
+              !this.state.token0Amount || 
+              !this.state.token1Amount || 
+              this.state.token0Error || 
+              this.state.token1Error ||
+              parseFloat(this.state.token0Amount || "0") * parseFloat(this.state.token1Amount || "0") <= 1000 * 1000
+            }>
+              Approve
+            </button>
+        }
+        {this.state.token0 && this.state.token1 && 
+          this.state.token0Amount && 
+          this.state.token1Amount && 
+          (parseFloat(this.state.token0Amount || "0") * parseFloat(this.state.token1Amount || "0") <= 1000 * 1000) ? 
+          <span className="err">* Product of tokens' amount has to be larger than 1000000.</span>
+        : null}
+      </div>
+    )
+  }
+};
+
+export default AddLiquidity;
---
+++ src/frontend/components/SwapRelated/LiquidityList.jsx
@@ -0,0 +1,298 @@
+import classNames from "classnames";
+import React, { Component } from "react";
+import { approveLpToken, getAllTokenPairs, getAllTokens, getLpAllowance, getLpBalance, removeLiquidity } from "../../APIs/token.js";
+import Icon from "../../stuff/Icon.jsx";
+import { currencyFormat } from "../../utils/common.js";
+import canister_ids from "../../utils/canister_ids.json";
+import "./LiquidityList.css";
+
+const DSWAP_CANISTER_ID = canister_ids.dswap.local;
+
+class LiquidityList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      tokens: [],
+      pairs: [],
+      onRemove: "",
+      requireUpdateBal: ""
+    };
+  }
+
+  _isMounted = false;
+  componentDidMount() {
+    this._isMounted = true;
+    this.initial();
+  }
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  initial = async () => {
+    Promise.all([
+      getAllTokens(),
+      getAllTokenPairs()
+    ])
+      .then(([res1, res2]) => {
+        console.log(res1, res2)
+        if (this._isMounted) this.setState({ 
+          tokens: res1 || [], 
+          pairs: res2 || [] 
+        });
+      })
+      .catch(err => {
+        console.log("fetch tokens or pairs failed");
+        console.log(err);
+      })
+      .finally(() => {
+        if (this._isMounted) this.setState({ loading: false });
+      });
+  };
+
+  render() {
+    const { tokens, pairs, loading, onRemove } = this.state;
+    return (
+      <div className="LiquidityList">
+        <div className="scroll-wrap">
+          <label className="main-title">Liquidity</label>
+          {loading ? 
+            <Icon name="spinner" spin />
+          : null}
+          {!loading && !pairs.length ? 
+            <span className="no-pair">No token-pair yet</span>
+          : null}
+          {pairs.map((i, index) => (
+            <LiquidityItem
+              key={index}
+              {...i}
+              token0={tokens.filter(el => el.canisterId === i.token0)[0] || {}}
+              token1={tokens.filter(el => el.canisterId === i.token1)[0] || {}}
+              onRemove={() => this.setState({ onRemove: i.id }, () => console.log(this.state.onRemove))}
+              hasToUpdateBal={this.state.requireUpdateBal === i.id}
+              reset={() => this.setState({ requireUpdateBal: "" })}
+            />
+          ))}
+          <RemoveLiquidityModal
+            ac={onRemove ? true : false}
+            pair={pairs.filter(i => i.id === onRemove)[0] || {}}
+            tokens={tokens}
+            close={() => this.setState({ onRemove: "" })}
+            triggerUpdate={() => this.setState({ requireUpdateBal: onRemove })}
+          />
+        </div>
+      </div>
+    ) 
+  }
+}
+
+export default LiquidityList;
+
+class LiquidityItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      bal: ""
+    };
+  }
+
+  _isMounted = false;
+  componentDidMount() {
+    this._isMounted = true;
+    this.getBalance();
+  }
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (!prevProps.hasToUpdateBal && this.props.hasToUpdateBal) {
+      this.getBalance();
+      setTimeout(() => {
+        this.props.reset();
+      }, 500);
+    }
+  }
+
+  getBalance = () => {
+    getLpBalance(this.props.id, localStorage.getItem("dfinance_current_user"))
+      .then(bal => {
+        console.log("lp balance: ", bal);
+        if (this._isMounted) this.setState({ bal });
+      })
+      .catch(err => {
+        console.log("get lp balance failed");
+        console.log(err);
+      });
+  };
+
+  render() {
+    const { id, token0, token1, onRemove } = this.props;  
+    const { bal } = this.state;
+    return (
+      <div className="LiquidityItem">
+        <label>{token0.symbol || "BTC"}-{token1.symbol || "BTC"}</label>
+        <span className="bal">{bal ? `Balance: ${currencyFormat(bal, "18")}` : ""}</span>
+        <span className="id">Id: {id}</span>
+        <button onClick={onRemove}>Remove Liquidity</button>
+      </div>
+    )
+  }
+};
+
+class RemoveLiquidityModal extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: "",
+      approved: false,
+      bal: "",
+      amount: "",
+      error: false
+    };
+  }
+
+  _isMounted = false;
+  componentDidMount() {
+    this._isMounted = true;
+    this.initial();
+  }
+  componentWillUnmount () {
+    this._isMounted = false;
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.pair.id !== this.props.pair.id) {
+      this.initial();
+    }
+  }
+
+  initial = () => {
+    if (this.props.pair.id) {
+      getLpBalance(this.props.pair.id, localStorage.getItem("dfinance_current_user"))
+        .then(bal => {
+          if (this._isMounted) this.setState({ bal });
+        })
+        .catch(err => {
+          console.log("get lp token balance failed");
+          console.log(err);
+        });
+      getLpAllowance(this.props.pair.id, localStorage.getItem("dfinance_current_user"), DSWAP_CANISTER_ID)
+        .then(res => {
+          if (parseFloat(res) > 0 && this._isMounted) {
+            this.setState({ approved: true });
+          }
+        })
+        .catch(err => {
+          console.log("get lp token allowance failed");
+          console.log(err);
+        });
+    }
+  };
+  updateBal = () => {
+    getLpBalance(this.props.pair.id, localStorage.getItem("dfinance_current_user"))
+      .then(bal => {
+        if (this._isMounted) this.setState({ bal });
+      })
+      .catch(err => {
+        console.log("get lp token balance failed");
+        console.log(err);
+      });
+  };
+  amountOnChange = e => {
+    const val = e.target.value;
+    const reg = new RegExp(/^[0-9\.]*$/);
+    if (val && !reg.test(val)) return;
+    this.setState({ 
+      amount: val, 
+      error: !val || parseFloat(val) > parseFloat(this.state.bal || "0") ? true : false 
+    });
+  };
+  max = () => {
+    this.setState({ amount: this.state.bal || "0" });
+  };
+  approve = () => {
+    this.setState({ loading: "Approving..." });
+    const MAX_AMOUNT = Number.MAX_SAFE_INTEGER;  // TODO
+    approveLpToken(this.props.pair.id, DSWAP_CANISTER_ID, MAX_AMOUNT) // TODO
+      .then(() => {})
+      .catch(err => {
+        console.log("approve lp token failed");
+        console.log(err);
+      })
+      .finally(() => {
+        if (this._isMounted) this.setState({ loading: "Done" }, () => {
+          setTimeout(() => {
+            if (this._isMounted) this.setState({ loading: "", approved: true });
+          }, 1500);
+        });
+      });
+  };
+  remove = () => {
+    this.setState({ loading: "Removing..." });
+    const { token0, token1 } = this.props.pair;
+    removeLiquidity(token0, token1, this.state.amount)
+      .then(res => {
+        this.props.triggerUpdate();
+        if (this._isMounted) this.updateBal();
+      })
+      .catch(err => {
+        console.log("remove liquidity failed");
+        console.log(err);
+      })
+      .finally(() => {
+        if (this._isMounted) this.setState({ loading: "Done" }, () => {
+          setTimeout(() => {
+            if (this._isMounted) this.setState({ loading: "" });
+          }, 1500);
+        })
+      });
+  };
+  close = () => {
+    this.setState({ 
+      loading: "",
+      approved: false,
+      bal: "",
+      amount: "",
+      error: false
+    });
+    this.props.close();
+  };
+  
+  render () {
+    const { bal, approved, amount, error, loading } = this.state;
+    const { ac, tokens, pair } = this.props;
+    const token0 = tokens.filter(i => i.canisterId === pair.token0)[0] || {};
+    const token1 = tokens.filter(i => i.canisterId === pair.token1)[0] || {};
+    return (
+      <div className={classNames("RemoveLiquidityModal", { ac })}>
+        <div className="bg"></div>
+        <div className="wrap">
+          <button className="close" onClick={this.close}>
+            <Icon name="close" />
+          </button>
+          <label className="label">Remove Liquidity</label>
+          <label className="sub-label">{token0.symbol}-{token1.symbol}</label>
+          <input 
+            className={classNames({ error })}
+            type="text"
+            placeholder="0.00"
+            value={amount}
+            onChange={this.amountOnChange}
+          />
+          <div className="balance-ctrl">
+            <span>{bal ? `Balance: ${currencyFormat(bal, "18")}` : ""}</span>
+            <button onClick={this.max}>Max</button>
+          </div>
+          {approved ? 
+            loading ? 
+              <button className="submit" disabled>{loading}</button> :
+              <button className="submit" disabled={!amount || error} onClick={this.remove}>Remove</button>
+          : 
+            loading ? 
+              <button className="submit" disabled>{loading}</button> :
+              <button className="submit" disabled={!pair || !amount || error} onClick={this.approve}>Approve</button>
+          }
+        </div>
+      </div>
+    )
+  }
+}
