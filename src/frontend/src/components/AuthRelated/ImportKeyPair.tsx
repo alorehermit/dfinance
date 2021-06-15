5c37f7ba02df4ab38c3fd16f72ee460ce3241c3b
amoslover <amoslover@163.com>
2021-06-15 11:14:14 +0800

    import identity from mnemonic

--- src/frontend/src/components/AuthRelated/ImportKeyPair.tsx
+++ src/frontend/src/components/AuthRelated/ImportKeyPair.tsx
@@ -14,6 +14,8 @@ import PwdForm from "./PwdForm";
 
 interface Props extends RouteComponentProps {}
 const ImportKeyPair = (props: Props) => {
+  const [type, setType] = useState("1");
+  const [mnemonic, setMnemonic] = useState("");
   const [principal, setPrincipal] = useState("");
   const [publicKey, setPublicKey] = useState("");
   const [privateKey, setPrivateKey] = useState("");
@@ -32,6 +34,21 @@ const ImportKeyPair = (props: Props) => {
     const str = keyIdentity.toJSON()[0]; // public key hex string
     setPrincipal(val);
     setPublicKey(str);
+  };
+  const importMnemonicWallet = () => {
+    const bip39 = require("bip39");
+    let seed = bip39.mnemonicToSeedSync(mnemonic);
+    seed = Array.from(seed);
+    seed = seed.splice(0, 32);
+    seed = new Uint8Array(seed);
+    const keyIdentity = Ed25519KeyIdentity.generate(seed);
+    const val = keyIdentity.getPrincipal().toString(); // principal
+    const [str1, str2] = keyIdentity.toJSON(); // public key and private key hex string
+    setPrivateKey(str2);
+    setTimeout(() => {
+      setPublicKey(str1);
+      setPrincipal(val);
+    }, 200);
   };
   const confirm = () => {
     dispatch(
@@ -55,26 +72,76 @@ const ImportKeyPair = (props: Props) => {
           </button>
         </div>
         <h1>Import Idenity from Private Key</h1>
-        <input
-          type="text"
-          placeholder="Private Key"
-          value={privateKey}
-          onChange={(e) => setPrivateKey(e.target.value)}
-        />
-        {principal ? (
-          <div className="text">
-            Account Id: {principalToAccountIdentifier(principal, 0)}
+        <label>Type :</label>
+        <select
+          value={type}
+          onChange={(e) => {
+            setType(e.target.value);
+            setMnemonic("");
+            setPrivateKey("");
+            setPublicKey("");
+            setPrincipal("");
+          }}
+        >
+          <option value="1">Private Key</option>
+          <option value="2">Mnemonic</option>
+        </select>
+        {type === "1" ? (
+          <div>
+            <label>Private Key :</label>
+            <textarea
+              value={privateKey}
+              onChange={(e) => setPrivateKey(e.target.value)}
+            />
+            {principal ? (
+              <div className="text">
+                Account Id: {principalToAccountIdentifier(principal, 0)}
+              </div>
+            ) : null}
+            {publicKey ? (
+              <div className="text">Public Key: {publicKey}</div>
+            ) : null}
+            <div className="btns">
+              <button
+                onClick={importWallet}
+                disabled={principal ? true : false}
+              >
+                Import
+              </button>
+              <button onClick={confirm} disabled={!principal}>
+                Next
+              </button>
+            </div>
           </div>
         ) : null}
-        {publicKey ? <div className="text">Public Key: {publicKey}</div> : null}
-        <div className="btns">
-          <button onClick={importWallet} disabled={principal ? true : false}>
-            Import
-          </button>
-          <button onClick={confirm} disabled={!principal}>
-            Next
-          </button>
-        </div>
+        {type === "2" ? (
+          <div>
+            <label>Mnemonic :</label>
+            <textarea
+              value={mnemonic}
+              onChange={(e) => setMnemonic(e.target.value)}
+            />
+            {principal ? (
+              <div className="text">
+                Account Id: {principalToAccountIdentifier(principal, 0)}
+              </div>
+            ) : null}
+            {publicKey ? (
+              <div className="text">Public Key: {publicKey}</div>
+            ) : null}
+            <div className="btns">
+              <button
+                onClick={importMnemonicWallet}
+                disabled={principal ? true : false}
+              >
+                Import
+              </button>
+              <button onClick={confirm} disabled={!principal}>
+                Next
+              </button>
+            </div>
+          </div>
+        ) : null}
       </div>
     );
   } else {
